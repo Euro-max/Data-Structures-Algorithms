f603da2f50180debdeb2ea2263626e7a1fb483a6
Ahmed Wael <83090095+Euro-max@users.noreply.github.com>
2025-07-10 06:12:49 -0700

    Update TreeADT.c

--- Trees ADT/TreeADT.c
+++ Trees ADT/TreeADT.c
@@ -157,5 +157,37 @@ Node*delete(Node*root,int key){
     return root;
   }
 }
+bool AreTreesIdentical(Node*root1,Node*root2){
+  if(!root1&&!root2) return true;
+  if(!root1||!root2) return false;
+  return (root1->data==root2->data)&&AreTreesIdentical(root1->left,root2->left)&&AreTreesIdentical(root1->right,root2->right);
+}
+bool isMirror(Node* leftSub, Node* rightSub) {
+    
+    // Both are null, so they are mirror images
+    if (leftSub == nullptr && rightSub == nullptr) 
+        return true;
+    
+    // One of them is null, so they aren't mirror images
+    if (leftSub == nullptr || rightSub == nullptr || 
+        			leftSub->data != rightSub->data) {
+        return false;
+    }
+    
+    // Check if the subtrees are mirrors
+    return isMirror(leftSub->left, rightSub->right) &&
+           isMirror(leftSub->right, rightSub->left);
+}
+
+bool isSymmetric(Node* root) {
+    
+    // If tree is empty, it's symmetric
+    if (root == nullptr) 
+        return true;
+    
+    // Check if the left and right subtrees are 
+  	// mirrors of each other
+    return isMirror(root->left, root->right);
+}
     
 }
